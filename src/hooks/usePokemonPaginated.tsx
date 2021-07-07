c2878d81f2051e087bed58eba257a24f6191427d
Edgar Gomez <edgar@ecomm.pro>
2021-07-07 18:45:57 +0200

    Added axios, minimal Home style, interfaces and types, custom hook to get Pokemons

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { pokemonApi } from "../api/pokemonApi";
+import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/pokemonInterfaces";
+
+export const usePokemonPaginated = () => {
+
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+  
+  const loadPokemons = async() => {
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  }
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(poke =>console.log(poke.name));
+  }
+
+  useEffect(() => {
+    loadPokemons();
+  }, [])
+}
