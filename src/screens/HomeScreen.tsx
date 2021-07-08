0eb3e0a5adab5d1b977c3f9e554f1753a9d08eb1
Edgar Gomez <edgar@ecomm.pro>
2021-07-08 16:46:04 +0200

    Finished custom hook to retrieve pokemons - Show pokemons in homepage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
 import { styles } from '../theme/appTheme'
 
 export const HomeScreen = () => {
 
   const {top} = useSafeAreaInsets();
+  const {isLoading, simplePokemonList, loadPokemons} = usePokemonPaginated();
 
   return (
     <>
@@ -13,11 +15,31 @@ export const HomeScreen = () => {
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-      <Text style={{
+      {/*<Text style={{
         ...styles.title,
         ...styles.globalMargin,
         top: top + 20,
-      }}>Pokedex</Text>
+      }}>Pokedex</Text>*/}
+
+      <FlatList
+        data={simplePokemonList}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(pokemon)=>pokemon.id}
+        renderItem={({item})=>(
+          <Image
+            source={{uri: item.picture}}
+            style={{
+              width: 100,
+              height: 100
+            }}
+          />
+        )}
+
+        //infinite scroll
+        onEndReached={loadPokemons}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={<ActivityIndicator style={{height:100}} size={20} color="grey" />}
+      />
     </>
   )
 }
